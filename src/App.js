38a51f9fbe9c87655b275d0c41d3186a860103e3
cristian08071996 <albertsevilla1996@gmail.com>
2020-12-04 22:04:55 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Crear app de venta y alquiler de viviendas
         </p>
         <a
           className="App-link"
